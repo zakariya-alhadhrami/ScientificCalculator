b5f4d1f877814ca2aac3c4f41e1f93b7d1180d6d
Zakariya AlHadhrami <zakarya601@gmail.com>
2025-07-20 16:12:31 +0400

    feat: adding the subtract , multiply and division method

--- ScientificCalc.java
+++ ScientificCalc.java
@@ -22,6 +22,15 @@ public class ScientificCalc {
     public static double Addition(double num1,double num2){
         return num1+num2;
     }
+    public static double Subtraction(double num1,double num2){
+        return num1-num2;
+    }
+    public static double Multiplication(double num1,double num2){
+        return num1*num2;
+    }
+    public static double Division(double num1,double num2){
+        return num1/num2;
+    }
 
 
 
